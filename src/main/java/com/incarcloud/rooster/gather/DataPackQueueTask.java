d9e7d2ab42be7d0ba403487030bacd5706e620e9
youbeiwuhuan <bbfan@incarcloud.com>
2017-06-12 15:20:33 +0800

    日志用逗号隔开多线程下日志不连贯解决

--- src/main/java/com/incarcloud/rooster/gather/DataPackQueueTask.java
+++ src/main/java/com/incarcloud/rooster/gather/DataPackQueueTask.java
@@ -80,13 +80,13 @@ public class DataPackQueueTask implements  Runnable{
             MqSendResult sendResult = host.getBigMQ().post(mqMsg);
 
             if (null == sendResult.getException()) {// 正常返回
-                s_logger.info("success send to MQ:" , sendResult.getData());
+                s_logger.info("success send to MQ:" + sendResult.getData());
 
                 ByteBuf resp = packWrap.getDataParser().createResponse(pack, ERespReason.OK);
                  packWrap.getChannel().writeAndFlush(resp);
 
             } else {
-                s_logger.error("failed send to MQ:" , sendResult.getException().getMessage());
+                s_logger.error("failed send to MQ:" + sendResult.getException().getMessage());
 
                 ByteBuf resp = packWrap.getDataParser().createResponse(pack, ERespReason.Failed);
                 packWrap.getChannel().writeAndFlush(resp);
